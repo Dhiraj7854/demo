4fa863d63b7adf70bacc153905eea3edf225b037
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-01-13 15:38:28 +0530

    Update 5th.c

--- 5th.c
+++ 5th.c
@@ -16,6 +16,7 @@ int main()
 	account.AccType = "Savings";
 	printf("1.Withdrawal\n2.deposit\n3.Display Balance\n4.exit\n");
 	int menu;
+	start:
 	printf("Enter your choice: ");
 	scanf("%d",&menu);
 	switch (menu){
@@ -26,25 +27,29 @@ int main()
 			{
 				account.AvlBalance -= money;
 				printf("Withdrawal Successfull!!!\n");
-				printf("Available balance: %.2f",account.AvlBalance);
+				printf("Available balance: %.2f\n",account.AvlBalance);
 			}
 			else
 			{
-				printf("You don't have the required balance");
+				printf("You don't have the required balance\n");
 			}
+			goto start;
 			break;
+			
 		case 2:
 			//float deposit;
 			printf("Enter the amount: ");
 			scanf("%f",&deposit);
 			account.AvlBalance += deposit;
-			printf("Available Balance: ",account.AvlBalance);
+			printf("Available Balance: \n",account.AvlBalance);
+			goto start;
 			break;
 			
 		case 3:
 			printf("Account Number: %d\n",account.AccNumber);
 			printf("Customer Name: %s\n",account.CustName);
-			printf("Available balance: %.2f",account.AvlBalance);
+			printf("Available balance: %.2f\n",account.AvlBalance);
+			goto start;
 			break;
 			
 		case 4:
